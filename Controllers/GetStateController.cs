18ef4ba0289be1b8415cff9f91da511a2ea5a853
mAzal-dev <muhd.azal@gmail.com>
2023-02-24 23:35:34 +0800

    First Commit

---
+++ Controllers/GetStateController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Malaysia_states.Model;
+
+namespace Malaysia_states.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetStateController : ControllerBase
+    {
+        [Obsolete]
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment;
+
+        [Obsolete]
+        public GetStateController(Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment= hostingEnvironment;
+        }
+
+        [Obsolete]
+        public rootStateDetails? GetState()
+        {
+            var rootPath = _hostingEnvironment.ContentRootPath;
+            var fullPath = Path.Combine(rootPath, "Data/All.json");
+
+            var jsonData = System.IO.File.ReadAllText(fullPath);
+
+            if (string.IsNullOrEmpty(jsonData)) 
+                return null;
+
+            rootStateDetails datalist = JsonConvert.DeserializeObject<rootStateDetails>(
+                value: jsonData);
+
+            if (datalist==null)
+            {
+                return null;
+            }
+            else
+            {
+                return datalist;
+            }
+
+            
+
+          
+
+            
+        }
+    }
+}
---
+++ Model/StateDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace Malaysia_states.Model
+{
+    public class StateDetails
+    {
+        public string administrative_division {get;set;}
+        public string state { get;set;}
+        public string capital { get; set; }
+        public string royal_capital { get; set; }
+        public int population { get; set; }
+        public int total_area { get; set; }
+        public string license_plate_prefix { get; set; }
+        public string phone_area_code { get; set; }
+        public string abbreviation { get; set; }
+        public string ISO { get; set; }
+        public string FIPS { get; set; }
+        public double HDI { get; set; }
+        public string region { get; set; }
+        public string head_of_state { get; set; }
+        public string head_of_government { get; set; }
+    }
+    public class rootStateDetails
+    {
+        public List<StateDetails> data { get; set; }
+    }
+}
